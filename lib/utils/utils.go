2652cffcdd4314f8b972aca5592950f5cb63be85
gianlucm76 <gianluca.mardente@gmail.com>
2022-10-27 10:04:40 -0700

    Add utils package

---
+++ lib/utils/utils.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2022. projectsveltos.io. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/discovery"
+	memory "k8s.io/client-go/discovery/cached"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/kubectl/pkg/scheme"
+)
+
+// GetUnstructured returns an unstructured given a []bytes containing it
+func GetUnstructured(object []byte) (*unstructured.Unstructured, error) {
+	request := &unstructured.Unstructured{}
+	universalDeserializer := scheme.Codecs.UniversalDeserializer()
+	_, _, err := universalDeserializer.Decode(object, nil, request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode k8s resource %.50s. Err: %w",
+			string(object), err)
+	}
+
+	return request, nil
+}
+
+// GetDynamicResourceInterface returns a dynamic ResourceInterface for the policy's GroupVersionKind
+func GetDynamicResourceInterface(config *rest.Config, policy *unstructured.Unstructured) (dynamic.ResourceInterface, error) {
+	if config == nil {
+		return nil, fmt.Errorf("rest.Config is nil")
+	}
+
+	dynClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	gvk := policy.GroupVersionKind()
+
+	dc, err := discovery.NewDiscoveryClientForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
+	mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return nil, err
+	}
+	var dr dynamic.ResourceInterface
+	if mapping.Scope.Name() == meta.RESTScopeNameNamespace {
+		// namespaced resources should specify the namespace
+		dr = dynClient.Resource(mapping.Resource).Namespace(policy.GetNamespace())
+	} else {
+		// for cluster-wide resources
+		dr = dynClient.Resource(mapping.Resource)
+	}
+
+	return dr, nil
+}
---
+++ lib/utils/utils_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2022. projectsveltos.io. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils_test
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/projectsveltos/libsveltos/lib/utils"
+)
+
+const (
+	viewClusterRole = `apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: %s
+rules:
+- apiGroups: [""] # "" indicates the core API group
+  resources: ["pods"]
+  verbs: ["get", "watch", "list"]`
+)
+
+var _ = Describe("utils ", func() {
+	It("GetUnstructured returns proper object", func() {
+		policy, err := utils.GetUnstructured([]byte(fmt.Sprintf(viewClusterRole, randomString())))
+		Expect(err).To(BeNil())
+		Expect(policy.GetKind()).To(Equal("ClusterRole"))
+	})
+
+	It("GetDynamicResourceInterface returns dynamic resource interface", func() {
+		policy, err := utils.GetUnstructured([]byte(fmt.Sprintf(viewClusterRole, randomString())))
+		Expect(err).To(BeNil())
+
+		dr, err := utils.GetDynamicResourceInterface(testEnv.Config, policy)
+		Expect(err).To(BeNil())
+
+		_, err = dr.Create(ctx, policy, metav1.CreateOptions{})
+		Expect(err).To(BeNil())
+
+		const timeout = 20 * time.Second
+		// Eventual loop so testEnv Cache is synced
+		Eventually(func() error {
+			currentClusterRole := &rbacv1.ClusterRole{}
+			return testEnv.Get(context.TODO(),
+				types.NamespacedName{Name: policy.GetName()}, currentClusterRole)
+		}, timeout, time.Second).Should(BeNil())
+	})
+})
