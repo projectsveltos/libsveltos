54058b7433673e6ed50ec413bdc30aec1d9081ce
mgianluc <mgianluca@cisco.com>
2024-03-05 14:47:33 +0100

    Introduce ClusterSet/Set
    A ClusterSet/Set allows to manage a group of clusters and dynamically
    select a subset for deployments based on specific criteria.
    
    It offers the following capabilities:
    
    - Selection: Clusters are chosen using a defined clusterSelector;
    - Capping: A field named maxReplicas specifies the maximum number of clusters to be
    selected from the pool matching the clusterSelector.
    - Failover: If a selected cluster becomes unavailable, another cluster from the
    matching pool will be automatically chosen to maintain the desired number of active
    clusters (up to the maxReplicas limit).
    
    A ClusterProfile can reference a ClusterSet by specifying its name.
    A Profile can reference a Set by specifying its name.
    
    The add-ons defined in the ClusterProfile will be deployed only to the
    currently selected clusters within the referenced ClusterSet/Set.
    This enables dynamic deployment management based on the available and
    healthy clusters in the set.
    
    This feature is particularly useful for scenarios where you want to:
    
    Implement active/passive failover:
    Create a ClusterSet/Set with maxReplicas: 1 and have it match two clusters in the clusterSelector.
    This ensures only one cluster is active at a time.
    If the active cluster goes down, the backup cluster will be automatically selected for deployments.

---
+++ api/v1alpha1/set_type.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2024. projectsveltos.io. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	// SetFinalizer allows SetReconciler to clean up resources associated with
+	// Set before removing it from the apiserver.
+	SetFinalizer = "setfinalizer.projectsveltos.io"
+
+	SetKind = "Set"
+)
+
+//+kubebuilder:object:root=true
+//+kubebuilder:resource:path=sets,scope=Namespaced
+//+kubebuilder:subresource:status
+
+// Set is the Schema for the sets API
+type Set struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   Spec   `json:"spec,omitempty"`
+	Status Status `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SetList contains a list of Set
+type SetList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Set `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Set{}, &SetList{})
+}
