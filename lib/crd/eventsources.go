01ed4648d0d309c7b62ce8f2d1f142239097914b
mgianluc <mgianluca@cisco.com>
2024-09-09 09:09:01 +0200

    Bump packages

--- lib/crd/eventsources.go
+++ lib/crd/eventsources.go
@@ -21,7 +21,7 @@ var EventSourceCRD = []byte(`apiVersion: apiextensions.k8s.io/v1
 kind: CustomResourceDefinition
 metadata:
   annotations:
-    controller-gen.kubebuilder.io/version: v0.16.1
+    controller-gen.kubebuilder.io/version: v0.16.2
   name: eventsources.lib.projectsveltos.io
 spec:
   group: lib.projectsveltos.io
