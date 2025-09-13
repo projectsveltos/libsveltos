e2979ac3144b5b4ad168adb4ea175ee14229a23d
Gianluca Mardente <52940363+gianlucam76@users.noreply.github.com>
2025-09-13 11:58:47 +0200

    Revert "(chore) postpone advancing to clusterAPI core v1beta2"

--- lib/k8s_utils/utils_suite_test.go
+++ lib/k8s_utils/utils_suite_test.go
@@ -28,9 +28,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/klog/v2"
-
-	//nolint:staticcheck // SA1019: We are unable to update the dependency at this time.
-	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 	"sigs.k8s.io/cluster-api/util"
 	ctrl "sigs.k8s.io/controller-runtime"
 
